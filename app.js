a801a6301b38223757ff8428f1641749eaf1c583
Henry <henrykc24@gmail.com>
2022-08-11 23:29:04 +0100

    Add app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
+const userRouter = require('./routes/userRoute');
+
+// SET UP APP
+const app = express();
+
+// USE MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/api/v1/users', userRouter);
+
+// HANDLE WRONG ENDPOINTS
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
