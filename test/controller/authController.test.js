3a848a01002aaf12b6becc0128c958e4b09c58eb
Henry <henrykc24@gmail.com>
2022-08-14 18:47:32 +0100

    Implement tests

--- test/controller/authController.test.js
+++ test/controller/authController.test.js
@@ -1,59 +1,15 @@
-// const { expect } = require('chai');
-// const request = require('supertest');
-// const app = require('../../app');
+const { expect } = require('chai');
+const request = require('supertest');
+const server = require('../../server');
+const deleteTestUser = require('../../utils/deleteTestUser');
 
-// const port = process.env.PORT || 4000;
+const deleteUser = async () => await deleteTestUser();
 
-// const { signup } = require('../../controller/authController');
 const DBConnection = require('../../utils/DBConnection');
 
-// describe('POST /signup', () => {
-//   before((done) => {
-//     DBConnection.connectToDatabase()
-//       .then(() => {
-//         app.listen(port, () => {
-//           console.log(`App running on port ${port}`); // eslint-disable-line no-console
-//         });
-//         done();
-//       })
-//       .catch((err) => done(err));
-//   });
+let addedNewTestUser = false;
 
-//   after((done) => {
-//     DBConnection.disconnectFromDatabase();
-//     done();
-//   });
-
-//   it('OK, creating a new user works', function () {
-//     this.timeout(10000);
-//     request(signup)
-//       .post('/signup')
-//       .send({
-//         name: 'John Doe',
-//         email: 'johndoe@gmail.com',
-//         password: '88888888',
-//         passwordConfirm: '88888888',
-//         messagesLeft: 0,
-//       })
-//       .then((res) => {
-//         const { body } = res;
-//         expect(body).to.contain.property('status');
-//         done();
-//       })
-//       .catch((err) => done(err));
-//   });
-// });
-
-const { should, use } = require('chai');
-const chai = require('chai');
-const chaiHTTP = require('chai-http');
-const server = require('../../server');
-
-// Assertion style
-should();
-use(chaiHTTP);
-
-describe('API', () => {
+describe('API Test', () => {
   before((done) => {
     DBConnection.connectToDatabase()
       .then(() => done())
@@ -65,12 +21,69 @@ describe('API', () => {
     done();
   });
 
-  it('Should register a user', () => {
-    chai
-      .request('http://localhost:4000')
-      .get('/api/users/')
-      .end((err, response) => {
-        console.log('res', response.body);
-      });
+  it('OK, recover password request works', (done) => {
+    request(server)
+      .post('/api/v1/users/forgot-password')
+      .send({ email: 'henrykc24@yahoo.com' })
+      .then((res) => {
+        addedNewTestUser = true;
+        const { body } = res;
+        // console.log(body);
+        expect(body.message).to.equal('Token sent to the mail');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, creating a new user works', (done) => {
+    request(server)
+      .post('/api/v1/users/signup')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: '88888888',
+        passwordConfirm: '88888888',
+        messagesLeft: 0,
+      })
+      .then((res) => {
+        addedNewTestUser = true;
+        const { body } = res;
+        expect(body).to.contain.property('status');
+        deleteTestUser();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, signing a user in works', (done) => {
+    request(server)
+      .post('/api/v1/users/login')
+      .send({
+        email: 'henrykc24@yahoo.com',
+        password: 'password12345',
+      })
+      .then((res) => {
+        addedNewTestUser = true;
+        const { body } = res;
+        // console.log(body);
+        expect(body.data.user.email).to.equal('henrykc24@yahoo.com');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, home route works', (done) => {
+    request(server)
+      .get('/')
+      .then((res) => {
+        addedNewTestUser = true;
+        const { body } = res;
+        // console.log(body);
+        expect(body.message).to.equal(
+          'Welcome to Cope Notes Mental Health Support API service!'
+        );
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
